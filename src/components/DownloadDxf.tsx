d4013f4c20e47477b89da0b59e101c402a2174bd
b0ock3r <57836351+rekcoob@users.noreply.github.com>
2025-01-10 10:17:43 +0100

    feat: dynamically generate horizontal and vertical lines in DXF

--- src/components/DownloadDxf.tsx
+++ src/components/DownloadDxf.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import Drawing from 'dxf-writer'
+import { useAppContext } from '../context/AppContext'
+import { useCalculatedLayout } from '../hooks/useCalculatedLayout'
 
 const DownloadDXF: React.FC = () => {
+  const { rectLength, rectWidth, formatLength, formatWidth } = useAppContext()
+
+  // const { cols1, rows1, total1, remainder, totalRectangles } =
+  const { cols1, rows1 } = useCalculatedLayout(
+    rectLength,
+    rectWidth,
+    formatLength,
+    formatWidth
+  )
+
   const generateDXF = () => {
     const dxf = new Drawing()
 
-    dxf.drawLine(0, 0, 100, 100)
+    // dxf
+    //   .drawLine(0, 0, cols1 * rectLength, 0)
+    //   .drawLine(0, rectWidth, cols1 * rectLength, rectWidth)
+    //   .drawLine(0, rectWidth * 2, cols1 * rectLength, rectWidth * 2)
+    //   .drawLine(0, rectWidth * 3, cols1 * rectLength, rectWidth * 3)
+
+    // Draw horizontal lines
+    for (let i = 0; i <= rows1; i++) {
+      const y = rectWidth * i
+      dxf.drawLine(0, y, cols1 * rectLength, y)
+    }
+
+    // Draw vertical lines
+    for (let j = 0; j <= cols1; j++) {
+      const x = rectLength * j
+      dxf.drawLine(x, 0, x, rows1 * rectWidth)
+    }
+
+    // dxf.drawLine(0, 0, 0, rows1 * rectWidth)
 
     // Convert the DXF to a string
     const dxfString = dxf.toDxfString()
@@ -25,8 +55,10 @@ const DownloadDXF: React.FC = () => {
 
   return (
     <div>
-      <h3>Download DXF File</h3>
-      <button onClick={generateDXF}>Download DXF</button>
+      {/* <h3>Download DXF File</h3> */}
+      <button className='download-btn' onClick={generateDXF}>
+        Download DXF
+      </button>
     </div>
   )
 }
